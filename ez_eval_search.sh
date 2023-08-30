7d784e68a8c5e6b125adc08751e81d1deb5a8e7c
Rickard Maus <hauntear@gmail.com>
2023-08-30 19:11:02 +0900

    update eval

---
+++ ez_eval_search.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+for i in $(seq 1 20); do
+    threshold=$(echo "scale=2; 1/20 * $i" | bc -l)
+    echo $threshold
+    ./tools/eval_dance.sh exps/$1/ $threshold
+    cp exps/$1/tracker/* TrackEval/data/trackers/mot_challenge/dancetrack-val/ultima/data/
+    python ./TrackEval/scripts/run_mot_challenge.py --BENCHMARK dancetrack --SPLIT_TO_EVAL val --TRACKERS_TO_EVAL ultima --METRICS HOTA Identity > ./results/results_$threshold.txt
+done
